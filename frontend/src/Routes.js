06e48892b688b30aa0e261cc7a8b509982e03543
lksad <it20605902@my.sliit.lk>
2023-05-06 22:56:29 +0530

    add hotel cards

---
+++ frontend/src/Routes.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Routes as AppRoutes, Route } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import { SessionAuth } from "supertokens-auth-react/recipe/session";
+import {
+  
+  AddHotel,
+  
+
+} from "./pages";
+
+const Routes = () => {
+  return (
+    <AppRoutes>
+      <Route path="" element={<Home />} />
+      <Route
+        path="/destinations/:id"
+        element={
+          <SessionAuth>
+            <Details />
+          </SessionAuth>
+        }
+      />
+      
+      <Route
+        path="/hotels"
+        element={
+          <SessionAuth>
+            <Hotels />
+          </SessionAuth>
+        }
+      />
+      <Route
+        path="/hotels/:id"
+        element={
+          <SessionAuth>
+            <HotelDetails />
+          </SessionAuth>
+        }
+      />
+     
+      
+      
+      <Route path="/admin/hotels/new" element={<AddHotel />} />
+      
+
+      {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+    </AppRoutes>
+  );
+};
+
+export default Routes;
